5e006c84d1c90cfda68f69c0edd0d8120049a300
sakhiimam3 <sakhiimam3411@gmail.com>
2023-06-09 08:25:23 -0700

    graphql-project

---
+++ data.js
@@ -0,0 +1,46 @@
+export const users = [
+  {
+    id: 1,
+    name: "sakhidad",
+    email: "sakhiimam3411@gmail.com",
+  },
+  {
+    id: 2,
+    name: "noor",
+    email: "noor@gmail.com",
+  },
+  {
+    id:3,
+    name: "humayun",
+    email: "humayun@gmail.com",
+  },
+];
+
+
+export const quotes = [
+  {
+      description:"this code created by  sakhidad",
+      by:1
+  },
+  {
+    description:"this code created by  sakhidad",
+    by:1
+  },
+  {
+    description:"this code created by  noor",
+    by:2
+    
+  },
+  {
+    description:"this code created by  noor",
+    by:3
+    
+  },
+];
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,50 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
+import { users ,quotes} from './data.js';
+
+
+const typeDefs = `
+  type User {
+    id:ID
+    name:String
+    email:String
+    quotes:[quotes]
+  }
+  
+  type quotes{
+        description :String
+        by:ID
+  }
+
+  type Query {
+    users: [User]
+    quotes:[quotes]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => users,
+    quotes :()=>quotes
+  },
+  User:{
+       quotes :(user)=>quotes.filter((quote)=>by === user.id)
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins:[ApolloServerPluginLandingPageLocalDefault()]
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 8000 },
+});
+
+
+console.log(`🚀  Server ready at: ${url}`);
+
+
+
